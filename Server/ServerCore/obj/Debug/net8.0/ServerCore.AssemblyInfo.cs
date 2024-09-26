add6b24e3c04e21dd9cad3db7dda0ad8aad6e110
Eunjoo <quaziii9gamedev@gmail.com>
2024-09-26 17:39:38 +0900

    [feat] Session 3

--- Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
+++ Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4863237916115ad516c662d6c42b3900e11f7465")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c956129a2b644f90a1452099292f76c6fcb83e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
