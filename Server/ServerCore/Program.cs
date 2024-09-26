09d96add4ba401edafde4715ff24197b750934d7
Eunjoo <quaziii9gamedev@gmail.com>
2024-09-26 14:29:33 +0900

    [feat]

---
+++ Server/ServerCore/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 문지기
+            Socket listenerSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 문지기 교육
+                listenerSocket.Bind(endPoint);
+
+                // 영업 시작
+                // backlog : 최대 대기수 
+                listenerSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    // 손님을 입장시킨다
+                    Socket clientSocket = listenerSocket.Accept();
+
+                    // 받는다
+                    byte[] recvBuff = new byte[1024];
+                    int recBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recBytes);
+                    Console.WriteLine($"[From Clinet ] {recvData}");
+
+                    // 보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+                    clientSocket.Send(sendBuff);
+
+                    // 쫓아낸다
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+
+        }
+    }
+}
