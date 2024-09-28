6267b34dabcfc45291c3fb27bbb463531c992004
eunjoo <quaziii9gamedev@gmail.com>
2024-09-28 17:56:11 +0900

    [feat] Packet Session

--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df8b90cce27ee9db55687d5681a227115235248")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69750589f2dd77aa4096695cc51891f25ab6388e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
