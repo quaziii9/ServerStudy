a29265d0573638d1c74232a48e19a81946e8d3ea
eunjoo <quaziii9gamedev@gmail.com>
2024-09-28 19:14:25 +0900

    [feat] Serialization 1,2

---
+++ Server/Server/ClientSession.cs
@@ -0,0 +1,126 @@
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public abstract class Packet
+    {
+        public ushort size;
+        public ushort packetId;
+
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
+    }
+
+    class PlayerInfoReq : Packet
+    {
+        public long playerId;
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketId.PlayerInfoReq;
+        }
+
+        public override void Read(ArraySegment<byte> s)
+        {
+            ushort count = 0;
+
+            //ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
+            count += 2;
+            //ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+            this.playerId = BitConverter.ToInt64(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+
+            // success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), packet.size);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.playerId);
+            count += 8;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+
+            if (success == false)
+                return null;
+
+            return SendBufferHelper.Close(count);
+        }
+    }
+
+    public enum PacketId
+    {
+        PlayerInfoReq = 1,
+        PlayerInfoOk = 2
+    }
+
+    class ClientSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            //Packet packet = new Packet() { size = 100, packetId = 10 };
+
+            //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            //byte[] buffer = BitConverter.GetBytes(packet.size);
+            //byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
+            //Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+            //Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+            //ArraySegment<byte> sendBuff =  SendBufferHelper.Close(buffer.Length + buffer2.Length);
+
+            // 100명
+            // 1 -> 이동패킷이 100명
+            // 100 -> 이동패킷이 100 ^ 100 = 1만
+
+            //byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+            //Send(sendBuff);
+            Thread.Sleep(5000);
+            Disconnect();
+        }
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            ushort count = 0;
+
+            ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+            count += 2;
+            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+            count += 2;
+
+            switch((PacketId) id)
+            {
+                case PacketId.PlayerInfoReq:
+                    {
+                        PlayerInfoReq p = new PlayerInfoReq();
+                        p.Read(buffer);
+                        Console.WriteLine($"Player InfoReq : {p.playerId}");
+                    }
+                    break;
+            }
+
+            Console.WriteLine($"RecvPacketId : {id}, Size {size}");
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes : {numOfBytes}");
+        }
+    }
+}
--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69750589f2dd77aa4096695cc51891f25ab6388e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6267b34dabcfc45291c3fb27bbb463531c992004")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
